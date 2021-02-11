bec1506d2d612f212c438cd2cd68fb09b12bd9f0
Alexander Riccio <test35965@gmail.com>
2021-02-11 18:44:45 -0500

    Try, fail to get this working directly
    I want to serve this correctly from the beginning, without running a node server for the frontend and a rails for the backend like people did in the bootcamp.

--- COVID_CO2_tracker/config/routes.rb
+++ COVID_CO2_tracker/config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+
+  # Route root to index, will be useful to serve react later as per: https://www.digitalocean.com/community/tutorials/how-to-set-up-a-ruby-on-rails-project-with-a-react-frontend
+  root 'homepage#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
