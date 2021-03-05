b280d94ae61897039cb9c4b0da6da8c9d22f48ba
Alexander Riccio <test35965@gmail.com>
2021-03-05 17:01:02 -0500

    Revert "Yeah, no I was pretty close methinks"
    This reverts commit 4e636565bf5f01340f5282f823142e9227b6ed61.

--- COVID_CO2_TRACKER/app/models/api/v1.rb
+++
@@ -1,5 +0,0 @@
-module Api::V1
-  def self.table_name_prefix
-    'api_v1_'
-  end
-end
--- COVID_CO2_TRACKER/config/routes.rb
+++ COVID_CO2_TRACKER/config/routes.rb
@@ -1,11 +1,6 @@
 # frozen_string_literal: true
 
 ::Rails.application.routes.draw do
-  namespace :api do
-    namespace :v1 do
-      resources :places
-    end
-  end
   # get 'auth/create'
   # get 'auth/destroy'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
