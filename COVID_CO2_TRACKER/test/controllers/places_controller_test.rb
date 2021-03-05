7255eef3fc0e5f341070e6624c305a4d7df19f57
Alexander Riccio <test35965@gmail.com>
2021-03-05 17:01:52 -0500

    FINE I'll do the namespacing manually.

---
+++ COVID_CO2_TRACKER/test/controllers/places_controller_test.rb
@@ -0,0 +1,38 @@
+require "test_helper"
+
+class PlacesControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @place = places(:one)
+  end
+
+  test "should get index" do
+    get places_url, as: :json
+    assert_response :success
+  end
+
+  test "should create place" do
+    assert_difference('Place.count') do
+      post places_url, params: { place: { google_place_id: @place.google_place_id, last_fetched: @place.last_fetched } }, as: :json
+    end
+
+    assert_response 201
+  end
+
+  test "should show place" do
+    get place_url(@place), as: :json
+    assert_response :success
+  end
+
+  test "should update place" do
+    patch place_url(@place), params: { place: { google_place_id: @place.google_place_id, last_fetched: @place.last_fetched } }, as: :json
+    assert_response 200
+  end
+
+  test "should destroy place" do
+    assert_difference('Place.count', -1) do
+      delete place_url(@place), as: :json
+    end
+
+    assert_response 204
+  end
+end
