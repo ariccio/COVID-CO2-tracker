1c2401dd911f2bb414e94c8f6ff90033f12c693b
Alexander Riccio <test35965@gmail.com>
2021-04-14 18:53:44 -0400

    note to self

--- app/controllers/api/v1/device_controller.rb
+++ app/controllers/api/v1/device_controller.rb
@@ -14,9 +14,13 @@ module Api
     class DeviceController < ApiController
       skip_before_action :authorized, only: [:show]
       def create
+
+        # find(*args): https://api.rubyonrails.org/v6.1.3.1/classes/ActiveRecord/FinderMethods.html#method-i-find
+        # "If one or more records cannot be found for the requested ids, then ActiveRecord::RecordNotFound will be raised"
         @model = ::Model.find(device_params.fetch(:model_id))
 
         # this should be in a validator class:
+        # TODO: check if @model.device is nil and then return error.
         if @model.device.where(serial: device_params.fetch(:serial)).count.positive?
           return render(
             json: {
