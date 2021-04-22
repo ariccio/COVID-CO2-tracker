8e4c4abb8ba202b45f0fa1e10826e3862aee756f
Alexander Riccio <test35965@gmail.com>
2021-04-22 19:02:34 -0400

    Update youtube url

--- co2_client/src/utils/UrlPath.tsx
+++ co2_client/src/utils/UrlPath.tsx
@@ -1,5 +1,5 @@
 
 // TODO: move to paths/urls or paths/api_urls?
 export const API_URL = "/api/v1";
-export const YOUTUBE_VIDEO_INSTRUCTIONS_URL = "https://youtu.be/i9BWoUp-vPk";
+export const YOUTUBE_VIDEO_INSTRUCTIONS_URL = "https://youtu.be/FhT3X9emqJk";
 export const ABOUT_ME_ARICCIO_URL = "https://about.me/ariccio";
