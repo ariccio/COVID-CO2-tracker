1d18074a265ef985b798b5073ef0df9624a16257
Alexander Riccio <test35965@gmail.com>
2021-05-26 17:30:58 -0400

    Looks like new jsx transform is active some places...

--- co2_client/src/App.tsx
+++ co2_client/src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+// import React from 'react';
 
 // import {useSelector, useDispatch} from 'react-redux';
 import {Route, Redirect, Switch, Link} from 'react-router-dom';
