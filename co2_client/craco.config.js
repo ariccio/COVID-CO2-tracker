8c1d3c3906b813e7cfb23e70e3a6f718b938b0a6
Alexander Riccio <test35965@gmail.com>
2021-08-06 18:30:42 -0400

    try craco

---
+++ co2_client/craco.config.js
@@ -0,0 +1,14 @@
+// craco.config.js
+const purgecss = require('@fullhuman/postcss-purgecss');
+
+module.exports = {
+  style: {
+    postcss: {
+      plugins: [
+        purgecss({
+          content: ['./src/**/*.html', './src/**/*.tsx', './src/**/*.ts'],
+        }),
+      ],
+    },
+  },
+};
