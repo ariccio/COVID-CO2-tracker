0e2d4106908efc52339b9cfe5836fa01017ae16e
Alexander Riccio <test35965@gmail.com>
2021-11-24 21:23:38 -0500

    leetle changes

--- co2_client/src/features/google/GoogleMaps.tsx
+++ co2_client/src/features/google/GoogleMaps.tsx
@@ -95,6 +95,18 @@ function isMobileFacebookBrowser(): boolean {
     // example user agent from sentry:
     //Mozilla/5.0 (iPhone; CPU iPhone OS 15_1_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/19B81 [FBAN/FBIOS;FBDV/iPhone13,2;FBMD/iPhone;FBSN/iOS;FBSV/15.1.1;FBSS/3;FBID/phone;FBLC/en_US;FBOP/5]
 
+
+    // Some curious checks suggested here: https://blog.tomayac.com/2019/12/09/inspecting-facebooks-webview/
+    const temp = (window as any).TEMPORARY;
+    const persistent = (window as any).PERSISTENT;
+    if (temp !== undefined) {
+        console.log(`window.TEMPORARY: ${temp}`);
+    }
+    if (persistent !== undefined) {
+        console.log(`window.PERSISTENT: ${persistent}`);
+    }
+
+
     const ua = window.navigator.userAgent;
     const FBAN = ua.match(/FBAN/);
     const FBIOS = ua.match(/FBIOS/);
@@ -147,6 +159,7 @@ const errorPositionCallback = (error: GeolocationPositionError_, geolocationInPr
         }
         if (error.message === userDeniedString) {
             console.log(`error.message === '${userDeniedString}', user probably denied the permissions request. Nothing to do!`);
+            alert(error.message);
             return;
         }
         //do nothing
