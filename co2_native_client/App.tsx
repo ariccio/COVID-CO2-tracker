26f0a75c83b5394610c9ceff7d43ac850e25d9c0
Alexander Riccio <test35965@gmail.com>
2022-01-07 04:46:26 -0500

    insomnia

--- co2_native_client/App.tsx
+++ co2_native_client/App.tsx
@@ -1,7 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, AppRegistry } from 'react-native';
 
 export default function App() {
+
+
+
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
