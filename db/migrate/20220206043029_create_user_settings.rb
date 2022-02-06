d36f77aeb972ab3c4310f3aaa6c25af104bbc36d
Alexander Riccio <test35965@gmail.com>
2022-02-05 23:54:57 -0500

    oh for fucks sake

--- db/migrate/20220206043029_create_user_settings.rb
+++ db/migrate/20220206043029_create_user_settings.rb
@@ -3,7 +3,7 @@ class CreateUserSettings < ActiveRecord::Migration[6.1]
     create_table(:user_settings) do |t|
       # https://thejspr.com/blog/add-reference-with-different-model-name-in-rails-5/
       t.references(:realtime_upload_place, null: false, foreign_key: {to_table: :places})
-      t.references(:realtime_upload_sub_location, null: false, foreign_key: {to_table: :sub_locations}})
+      t.references(:realtime_upload_sub_location, null: false, foreign_key: {to_table: :sub_locations})
 
       t.timestamps
     end
