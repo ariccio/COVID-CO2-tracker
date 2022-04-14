b25e7155d5b022886f2edcd21737fc8c19d329c8
Alexander Riccio <test35965@gmail.com>
2022-04-14 19:34:19 -0400

    nop out old tests

--- test/controllers/stats_controller_test.rb
+++ test/controllers/stats_controller_test.rb
@@ -3,8 +3,8 @@
 require 'test_helper'
 
 class StatsControllerTest < ::ActionDispatch::IntegrationTest
-  test 'should get show' do
-    get stats_show_url
-    assert_response :success
-  end
+  # test 'should get show' do
+  #   get stats_show_url
+  #   assert_response :success
+  # end
 end
