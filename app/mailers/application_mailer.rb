ariccio/COVID-CO2-tracker

commit 65f663429fbc45c85d8a81db1a5345c61ef0d82b

yay tests! also try speed up a bit

config/environments/production.rb
@@ -33,7 +33,7 @@ require 'active_support/core_ext/integer/time'
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
-  config.active_storage.service = :local
+  # config.active_storage.service = :local
 
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil

Gemfile
@@ -29,6 +29,12 @@ gem 'rack-cors'
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw, :mswin]
+end
+
+group :test do
+  gem 'database_cleaner-active_record'
+  gem 'faker'
+  gem 'shoulda-matchers'
   gem 'rspec-rails'
   gem 'factory_bot_rails'
   gem 'jsonapi-rspec'

app/channels/application_cable/channel.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-module ApplicationCable
-  class Channel < ::ActionCable::Channel::Base
-  end
-end
+# module ApplicationCable
+#   class Channel < ::ActionCable::Channel::Base
+#   end
+# end

app/channels/application_cable/connection.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-module ApplicationCable
-  class Connection < ::ActionCable::Connection::Base
-  end
-end
+# module ApplicationCable
+#   class Connection < ::ActionCable::Connection::Base
+#   end
+# end

app/controllers/api/v1/auth_controller.rb
@@ -158,7 +158,8 @@ module Api
         if Rails.env === 'test'
           ::Rails.logger.warn("test auth path")
           # No encryption for test env
-          @decoded_token = {sub: user_login_google_params[:id_token]}
+          # byebug
+          @decoded_token = params['user']
         else
           @decoded_token = token_from_google
         end

bin/rails
@@ -1,4 +1,5 @@
 #!/usr/bin/env ruby.exe
+puts "Start of bin/rails: #{Time.now}"
 APP_PATH = File.expand_path('../config/application', __dir__)
 require_relative '../config/boot'
 require 'rails/commands'

config/application.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
-
+puts "Start of config/application.rb: #{Time.now}"
 require_relative 'boot'
 
 require 'rails'
@@ -7,13 +7,13 @@ require 'rails'
 require 'active_model/railtie'
 require 'active_job/railtie'
 require 'active_record/railtie'
-require 'active_storage/engine'
+# require 'active_storage/engine'
 require 'action_controller/railtie'
-require 'action_mailer/railtie'
-require 'action_mailbox/engine'
-require 'action_text/engine'
+# require 'action_mailer/railtie'
+# require 'action_mailbox/engine'
+# require 'action_text/engine'
 require 'action_view/railtie'
-require 'action_cable/engine'
+# require 'action_cable/engine'
 require 'sprockets/railtie'
 require 'rails/test_unit/railtie'
 
@@ -23,6 +23,7 @@ require 'rails/test_unit/railtie'
 
 module COVIDCo2Tracker
   class Application < ::Rails::Application
+    puts "Start Application class: #{Time.now}"
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults(6.1)
 
@@ -42,15 +43,19 @@ module COVIDCo2Tracker
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
 
+    # puts "Starting middleware: #{Time.now}"
     config.middleware.use(::ActionDispatch::Cookies)
 
     # Middleware for ActiveAdmin
     config.middleware.use(::Rack::MethodOverride)
     config.middleware.use(::ActionDispatch::Flash)
     config.middleware.use(::ActionDispatch::Session::CookieStore)
+    # puts "End      middleware: #{Time.now}"
 
     # Fix rails g scaffold for ActiveAdmin
     # As per https://blog.heroku.com/a-rock-solid-modern-web-stack
     config.app_generators.scaffold_controller = :scaffold_controller
+    puts "End   Application class: #{Time.now}\r\n"
   end
 end
+puts "end   of config/application.rb: #{Time.now}\r\n"

config/boot.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
+puts "Start of config/boot.rb: #{Time.now}"
 
 ::ENV['BUNDLE_GEMFILE'] ||= ::File.expand_path('../Gemfile', __dir__)
 
 require 'bundler/setup' # Set up gems listed in the Gemfile.
+puts "bootsnap setup: #{Time.now}"
 require 'bootsnap/setup' # Speed up boot time by caching expensive operations.
+puts "end   of config/boot.rb: #{Time.now}\r\n"
+

commit 2113c09362475827f931a19bc1cb44964537d224

fix brokie
I broke in 65f663429fbc45c85d8a81db1a5345c61ef0d82b

I nopped it out for speed, but forget to comment out the decls:
https://github.com/ariccio/COVID-CO2-tracker/commit/65f663429fbc45c85d8a81db1a5345c61ef0d82b#diff-c1fd91cb1911a0512578b99f657554526f3e1421decdb9e908712beab57e10f9

app/mailers/application_mailer.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class ApplicationMailer < ::ActionMailer::Base
-  default from: 'from@example.com'
-  layout 'mailer'
-end
+# class ApplicationMailer < ::ActionMailer::Base
+#   default from: 'from@example.com'
+#   layout 'mailer'
+# end

config/environments/production.rb
@@ -57,7 +57,7 @@ require 'active_support/core_ext/integer/time'
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "COVID_CO2_TRACKER_production"
 
-  # config.action_mailer.perform_caching = false
+  config.action_mailer.perform_caching = false
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.