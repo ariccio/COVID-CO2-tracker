da17e8b63e1193205681508963ae608c0e978a97
Alexander Riccio <test35965@gmail.com>
2022-07-14 15:25:15 -0400

    make script exit fail to stop push

--- .lefthook/pre-push/check_validates_timeliness.rb
+++ .lefthook/pre-push/check_validates_timeliness.rb
@@ -2,7 +2,6 @@ require 'rubygems'
 require 'gems'
 
 # require "http"
-
 # RUBYGEMS_API_BASE_URL = 'https://rubygems.org/api/v2/rubygems/'
 
 CURRENT_VERSION = '6.0.0.beta2'
@@ -17,6 +16,9 @@ def run
     correct_latest_version = Gems.versions(VALIDATES_TIMELINESS)[0]["number"]
     if (correct_latest_version != CURRENT_VERSION)
         puts "Possible new version available: #{correct_latest_version}"
+        
+        # Lefthook will treat this as a fail and stop the push.
+        exit(1)
         return
     end
     puts "Probably no new version of #{VALIDATES_TIMELINESS} available."
