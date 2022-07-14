d41df650e7c2ffeed8dec511c908484290bcc5a3
Alexander Riccio <test35965@gmail.com>
2022-07-14 14:38:47 -0400

    tweak check_validates_timeliness

--- gitscripts/check_validates_timeliness.rb
+++ gitscripts/check_validates_timeliness.rb
@@ -6,6 +6,7 @@ require 'gems'
 # RUBYGEMS_API_BASE_URL = 'https://rubygems.org/api/v2/rubygems/'
 
 CURRENT_VERSION = '6.0.0.beta2'
+VALIDATES_TIMELINESS = 'validates_timeliness'
 # CURRENT_VERSION = 'fartipelago'
 # def check_validates_timeliness
 #     validates_timeliness_in_rubygems = "#{RUBYGEMS_API_BASE_URL}/validates_timeliness"
@@ -13,10 +14,12 @@ CURRENT_VERSION = '6.0.0.beta2'
 
 def run
 
-    correct_latest_version = Gems.versions('validates_timeliness')[0]["number"]
+    correct_latest_version = Gems.versions(VALIDATES_TIMELINESS)[0]["number"]
     if (correct_latest_version != CURRENT_VERSION)
         puts "Possible new version available: #{correct_latest_version}"
+        return
     end
+    puts "Probably no new version of #{VALIDATES_TIMELINESS} available."
     # pp "\"latest_version\" #{Gems.latest_version('validates_timeliness')}"
 end
 
