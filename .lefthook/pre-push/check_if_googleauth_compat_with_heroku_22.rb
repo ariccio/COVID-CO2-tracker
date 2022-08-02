ec40f6d6bba97039628b68994514308e6f4a7670
Alexander Riccio <test35965@gmail.com>
2022-08-02 18:27:37 -0400

    create script to check if new version of googleauth available

---
+++ .lefthook/pre-push/check_if_googleauth_compat_with_heroku_22.rb
@@ -0,0 +1,25 @@
+require 'rubygems'
+require 'gems'
+
+
+CURRENT_VERSION = '1.2.0'
+GEM_NAME = 'googleauth'
+
+
+
+def run
+    correct_latest_version = Gems.versions(GEM_NAME)[0]["number"]
+    
+    if (correct_latest_version == CURRENT_VERSION)
+        puts "Probably no new version of #{GEM_NAME} available."
+        pp "\"latest_version\" #{Gems.latest_version('GEM_NAME')}"
+        return
+    end
+    
+    puts "Possible new version #{GEM_NAME} available: #{correct_latest_version}"
+    # Lefthook will treat this as a fail and stop the push.
+    exit(1)
+end
+
+run
+
