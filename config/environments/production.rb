989f9e1511f22086d9820261e80605616fd90a76
Alexander Riccio <test35965@gmail.com>
2022-10-18 15:41:18 -0400

    try to force heroku redeploy

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -5,6 +5,8 @@ require 'active_support/core_ext/integer/time'
 ::Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  puts "booting in production mode/env!"
+
   # Code is not reloaded between requests.
   config.cache_classes = true
 
