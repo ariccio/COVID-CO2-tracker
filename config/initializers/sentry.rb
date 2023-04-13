7817db5576bfb99dca6ef75d27ee5dc81f8818d5
Alexander Riccio <test35965@gmail.com>
2023-04-13 14:29:22 -0400

    turn on sentry perf monitoring, yesterday lol

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -2,14 +2,14 @@
 
 ::Sentry.init do |config|
   config.dsn = 'https://5c72ea76ca204179b35fa8a3eb847ab0@o584271.ingest.sentry.io/5737166' unless ((Rails.env == "development") || (Rails.env == "test") )
-  config.breadcrumbs_logger = [:active_support_logger]
+  config.breadcrumbs_logger = [:sentry_logger, :active_support_logger, :http_logger]
 
 
   config.environment = Rails.env
   config.enabled_environments = ["production"]
   # To activate performance monitoring, set one of these options.
   # We recommend adjusting the value in production:
-  # config.traces_sample_rate = 0
+  config.traces_sample_rate = 0
   # or
   # config.traces_sampler = lambda do |context|
   #   true
