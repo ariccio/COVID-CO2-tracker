e11ad8595958650495bcfa6c191cebbe5b29be3b
Alexander Riccio <test35965@gmail.com>
2023-05-08 21:05:24 -0400

    gotta work more on this. Feels good to be back.

--- co2_client/cypress/e2e/co2trackers_frontend/not_logged_in_try_device.cy.ts
+++ co2_client/cypress/e2e/co2trackers_frontend/not_logged_in_try_device.cy.ts
@@ -6,6 +6,6 @@ describe('try to create a device when not logged in', () => {
         cy.visit('http://localhost:3001/devices');
     })
     it ('renders something useful when not logged in', () => {
-        cy.contains("Add your devices and view stats");
+        cy.contains("Add your devices and view stats").should("not.exist");
     })
 })
