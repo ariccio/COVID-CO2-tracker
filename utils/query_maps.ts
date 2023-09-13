d6764e5bb9474989e347882c8dda84ddc42f6cc3
Alexander Riccio <test35965@gmail.com>
2023-09-12 23:27:04 -0400

    querying for OFFLINE analysis
    google I'm trying to make the world a better place and not break your API terms pls

--- utils/query_maps.ts
+++ utils/query_maps.ts
@@ -1,11 +1,81 @@
-import { cleanFile } from "./clean_places_google_ids";
-import {Client} from "@googlemaps/google-maps-services-js";
+import * as fs from 'fs';
 
+import { cleanFile } from "./clean_places_google_ids";
+import {Client, PlaceDetailsRequest, PlaceDetailsResponse} from "@googlemaps/google-maps-services-js";
+
+
+function placeDetailsRequestForPlace(key: string, id: string): PlaceDetailsRequest {
+    return {
+        params: {
+            key: key,
+            place_id: id,
+            fields: ['type']
+        }
+    };
+}
+
+
+function successfulPlaceDetailsRequest(value: PlaceDetailsResponse) {
+    console.log(`response succeeded, data:`);
+    if (value.data.result === undefined) {
+        console.log("resutls undefined");
+        return;
+    }
+    if (!Array.isArray(value.data.result)) {
+        // console.log(`result ${JSON.stringify(value.data.result)} not array.`);
+        if (value.data.result.address_components !== null) {
+            delete value.data.result.address_components;
+        }
+        if (value.data.result.photos !== null) {
+            delete value.data.result.photos;
+        }
+        if (value.data.result.reviews !== null) {
+            delete value.data.result.reviews;
+        }
+
+        Object.entries(value.data.result).forEach(([key, value]) => {
+            if (typeof(value) === "object") {
+                console.log(`key ${key}, object as JSON instead: ${JSON.stringify(value)}`)
+            }
+            else {
+                console.log(`key '${key}', value: '${value}'`); // "a 5", "b 7", "c 9"
+            }
+        })
+
+        return;
+    }
+
+    if (value.data.result.length === 0) {
+        console.log("empty result");
+        return;
+    }
+    for (let i = 0; i < value.data.result[0].data.keys.length; ++i) {
+        console.log(`key ${i}: ${value.data.result[0].keys[i]}`);
+    }
+    // console.table(value.data);
+    console.log(`response succeeded, data: ${JSON.stringify(value.data)}`);
+
+}
 
 
 function main() {
-    const ids = cleanFile(process.argv[2]);
+    const originalFile = process.argv[2];
+    const ids = cleanFile(originalFile);
     console.log(ids);
+
+    // console.log(ids[0]);
+    const id = String(ids[0]);
+    console.log(`querying ${id}...`);
+
+    const key = fs.readFileSync(`analysis_key.key`).toString();
+    const client = new Client({});
+    const request: PlaceDetailsRequest = placeDetailsRequestForPlace(key, id);
+    
+    // https://developers.google.com/maps/documentation/places/web-service/details
+    client.placeDetails(request).then(successfulPlaceDetailsRequest)
+    .catch((e) => {
+        console.log(e.response.data.error_message);
+    });
 }
 
 
