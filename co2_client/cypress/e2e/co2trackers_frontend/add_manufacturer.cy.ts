0386029f22604685aaa2d8bcf864e1331c02e69f
Alexander Riccio <test35965@gmail.com>
2024-02-26 15:32:13 -0500

    tweak

--- co2_client/cypress/e2e/co2trackers_frontend/add_manufacturer.cy.ts
+++ co2_client/cypress/e2e/co2trackers_frontend/add_manufacturer.cy.ts
@@ -4,7 +4,7 @@ describe('Add manufacturer', () => {
         cy.loginByGoogleApi();
         cy.request('http://localhost:3002/cypress_rails_reset_state')
     })
-    it('can create and select manufacturer', () => {
+    it('cannot create an extant manufacturer', () => {
         const spy = cy.spy(window, 'alert');
         cy.visit('/');
         cy.visit('http://localhost:3001/devices');
@@ -26,16 +26,30 @@ describe('Add manufacturer', () => {
         cy.contains("Add a manufacturer to the database").should('exist');
         
         //try again:
-        cy.get('div.fade.modal.show > div > div > div.modal-body > form > input').type('blaaarghhh');
+        // cy.get('div.fade.modal.show > div > div > div.modal-body > form > input').type('blaaarghhh');
 
 
-        cy.contains('Submit').click();
+        // cy.contains('Submit').click();
         
-        // cy.get('div.fade.modal.show > div > div > div.modal-footer').get('.spinner-border').should('be.visible');
-        // should fail:
-        cy.contains('Please reduce administrative burden').should('not.exist');
-        cy.contains("Add a manufacturer to the database").should('not.exist');
-        
+        // // cy.get('div.fade.modal.show > div > div > div.modal-footer').get('.spinner-border').should('be.visible');
+        // // should fail:
+        // cy.contains('Please reduce administrative burden').should('not.exist');
+        // cy.contains("Add a manufacturer to the database").should('not.exist');
+        })
+        it('can create a new manufacturer', () => {
+            const spy = cy.spy(window, 'alert');
+            cy.visit('/');
+            cy.visit('http://localhost:3001/devices');
+            cy.contains("Add your devices and view stats").should("be.visible");
+            cy.contains("Select manufacturer").should("be.visible");
+            cy.contains("Select manufacturer").click();
+            cy.contains("Create new manufacturer").click();
+            cy.contains('Please reduce administrative burden').should('exist');
+            cy.contains("Add a manufacturer to the database").should('exist');
+            cy.get('div.fade.modal.show > div > div > div.modal-body > form > input').type('blaaarghhh');
+            cy.contains('Submit').click();
 
+            cy.contains('Please reduce administrative burden').should('not.exist');
+            cy.contains("Add a manufacturer to the database").should('not.exist');    
         })
 })
