b04b33390a2ed69f6df407aa92e4e903d4977b7a
Alexander Riccio <test35965@gmail.com>
2024-04-03 17:09:31 -0400

    cleanup

--- utils/run_e2e.ts
+++ utils/run_e2e.ts
@@ -9,14 +9,17 @@ import {exec, SubProcess, SubProcessOptions} from 'teen_process';
 console.log(`run_e2e.ts`);
 
 type procHandle = SubProcess | undefined;
+type procPid =  number | undefined;
+const DEFAULT_RAILS_PORT = '3000';
 
-const DEFAULT_RAILS_PORT = 3000;
-
-const DEFAULT_FRONTEND_PORT = 3001;
+const DEFAULT_FRONTEND_PORT = '3001';
 
 let cypress_rails: procHandle;
+let cypress_rails_pid: procPid;
+
 // let cypress: procHandle;
 let webpack: procHandle;
+let webpack_pid: procPid;
 
 
 type startDetector = ((stdout: string, stderr: string) => boolean) | number | null
@@ -113,11 +116,11 @@ function checkPorts() {
         shell: true
     }
     try {
-        const result3000 = child_process.spawnSync('lsof', [`-i :3000 -P`], spawnOpts);
+        const result3000 = child_process.spawnSync('lsof', [`-i :${DEFAULT_RAILS_PORT} -P`], spawnOpts);
         if (result3000.status === 0) {
             // console.log(`current rails pid: ${rails?.pid}`);
             console.log(`current frontend pid (checkPorts): ${cypress_rails?.pid}`);
-            console.log(`processes on 3000: (checkPorts)\r\n${result3000.output.toString()}`);
+            console.log(`processes on ${DEFAULT_RAILS_PORT}: (checkPorts)\r\n${result3000.output.toString()}`);
         }
         else {
             console.warn(`lsof failed (checkPorts): ${result3000.status}`);
@@ -125,11 +128,11 @@ function checkPorts() {
             console.log(`lsof output anyways (checkPorts): ${result3000.output.toString()}`);
         }
 
-        const result3001 = child_process.spawnSync('lsof', [`-i :3001 -P`], spawnOpts);
+        const result3001 = child_process.spawnSync('lsof', [`-i :${DEFAULT_FRONTEND_PORT} -P`], spawnOpts);
         if (result3001.status === 0) {
             // console.log(`current rails pid: ${rails?.pid}`);
             console.log(`current frontend pid (checkPorts): ${cypress_rails?.pid}`);
-            console.log(`process on 3001 (checkPorts):\r\n${result3001.output.toString()}`);
+            console.log(`process on ${DEFAULT_FRONTEND_PORT} (checkPorts):\r\n${result3001.output.toString()}`);
         }
         else {
             console.warn(`lsof failed (checkPorts): ${result3001.status}`);
@@ -144,6 +147,11 @@ function checkPorts() {
 }
 
 const cypressRailsStartDetector: startDetector = (stdout, stderr) => {
+    if ((cypress_rails_pid === undefined) && (cypress_rails !== undefined) && (cypress_rails.pid !== undefined) && (cypress_rails.pid !== null)) {
+        console.log(`cypress_rails pid: ${cypress_rails.pid}`);
+        console.log(`Setting cypress_rails_pid global: ${cypress_rails.pid}`);
+        cypress_rails_pid = cypress_rails.pid;
+    }
     // if (stderr.length > 0) {
     //     // console.warn(`stdout for frontend is > 0 (stderr: '${stderr}') assuming failure.`);
     //     // throw new Error(`stdout for frontend is > 0, assuming failure.`);
@@ -175,6 +183,12 @@ const cypressRailsStartDetector: startDetector = (stdout, stderr) => {
 }
 
 const webpackStartDetector: startDetector = (stdout, stderr) => {
+
+    if ((webpack_pid === undefined) && (webpack !== undefined) && (webpack.pid !== undefined) && (webpack.pid !== null)) {
+        console.log(`webpack pid: ${webpack.pid}`);
+        console.log(`Setting webpack_pid global: ${webpack.pid}`);
+        webpack_pid = webpack.pid;
+    }
     if (/ERROR in/.test(stdout)) {
         console.log("(stdout) Possible compilation error!");
         throw new Error("(stdout) some kind of error - maybe compilation?");
@@ -244,14 +258,14 @@ function sleep(ms: number) {
 async function main() {
     console.log('\n\n\n\n');
 
-    const is3000Clear = checkPortClear('3000');
+    const is3000Clear = checkPortClear(DEFAULT_RAILS_PORT);
     if (!is3000Clear) {
-        console.log(`port 3000 is not clear! Exiting.`);
+        console.log(`port ${DEFAULT_RAILS_PORT} is not clear! Exiting.`);
         return 1;
     }
-    const is3001Clear = checkPortClear('3001');
+    const is3001Clear = checkPortClear(DEFAULT_FRONTEND_PORT);
     if (!is3001Clear) {
-        console.log(`port 3001 is not clear! Exiting.`);
+        console.log(`port ${DEFAULT_FRONTEND_PORT} is not clear! Exiting.`);
         return 1;
     }
 
@@ -260,12 +274,12 @@ async function main() {
 
     // rails = execSh.promise("rails s", undefined)
     let backendEnv = JSON.parse(JSON.stringify(process.env));
-    backendEnv.PORT = '3000';
+    backendEnv.PORT = DEFAULT_RAILS_PORT;
     // backendEnv.RAILS_MAX_THREADS = 7;
     // backendEnv.ENV = 'test';
     backendEnv.IsEndToEndBackendServerSoSTFUWithTheLogs = 'yes';
     backendEnv.CYPRESS_RAILS_CYPRESS_DIR = './co2_client';
-    backendEnv.CYPRESS_RAILS_PORT = '3000';
+    backendEnv.CYPRESS_RAILS_PORT = DEFAULT_RAILS_PORT;
     backendEnv.CYRESS_RAILS_HOST = 'localhost';
     // backendEnv.CYPRESS_RAILS_OVERRIDE_FULL_BASE_PATH = 'http://localhost:3001/';
     // backendEnv.CYPRESS_RAILS_BASE_PATH = 
@@ -277,7 +291,7 @@ async function main() {
     // rake --trace=stdout --verbose --backtrace=stdout cypress:run 
     cypress_rails = new SubProcess('rake cypress:run', undefined, rails_opts);
     setupFollowerHooks(cypress_rails, "cypress_rails");
-
+    
     const dump = (value: {stderr: string, stdout: string}) => {
         console.log('----------------------');
         console.log(value.stderr);
@@ -301,7 +315,7 @@ async function main() {
     // };
 
     let webpackEnv = JSON.parse(JSON.stringify(process.env));
-    webpackEnv.PORT = '3001';
+    webpackEnv.PORT = DEFAULT_FRONTEND_PORT;
     webpackEnv.BROWSER = 'none';
 
     const webpack_opts: SubProcessOptions = {
@@ -317,8 +331,11 @@ async function main() {
     // setupFollowerHooks(cypress, "cypress");
 
 
+    console.warn("TODO: .start can reject! See utils/node_modules/teen_process/lib/subprocess.js:200");
+    //see also: https://nodejs.org/api/child_process.html#child_processspawncommand-args-options
+    // and: https://github.com/libuv/libuv/blob/17219b8f39f7cd33472c94214010b603322bd0fa/src/unix/process.c#L956
     await webpack.start(webpackStartDetector);
-    const webPackPortInUse = checkPortInUse('3001');
+    const webPackPortInUse = checkPortInUse(DEFAULT_FRONTEND_PORT);
     if (webPackPortInUse) {
         console.log("Ok, webpack's port seems to be in use...");
     }
@@ -326,11 +343,14 @@ async function main() {
         console.log("Webpack's port is not in use? Exiting...");
         return 1;
     }
+    console.warn("TODO: .start can reject! See utils/node_modules/teen_process/lib/subprocess.js:200");
+    // see also: https://nodejs.org/api/child_process.html#child_processspawncommand-args-options
+    // and: https://github.com/libuv/libuv/blob/17219b8f39f7cd33472c94214010b603322bd0fa/src/unix/process.c#L956
     await cypress_rails.start(cypressRailsStartDetector);
     // await cypress.start(frontendStartDetector, 60_000);
     console.log(`cypress running...`);
     
-    const cypressRailsPortInUse = checkPortInUse('3000');
+    const cypressRailsPortInUse = checkPortInUse(DEFAULT_RAILS_PORT);
     if (cypressRailsPortInUse) {
         console.log("Ok, cypress_rails's port seems to be in use...");
     }
@@ -463,8 +483,45 @@ function exceptionDump(e_: unknown) {
     }
 }
 
-function forceClose(result: number) {
-    
+function forceClose(pid: number | undefined) {
+    if (pid === undefined) {
+        console.log(`pid is undefined! Nothing to close.`);
+        return;
+    }
+    // https://nodejs.org/api/process.html#processkillpid-signal:~:text=in%20Worker%20threads.-,process.kill(pid%5B%2C%20signal%5D),-%23
+    console.log(`force closing pid ${pid}`);
+    const err = process.kill(pid);
+
+
+    // internally, calls one of two things:
+    // 1: 
+    // https://github.com/nodejs/node/blob/4df34cf6dd497c4b7487c98fa5581c85b05063dc/src/process_wrap.cc#L300
+    // which calls, uv_process_kill, which is just uv_kill: https://github.com/libuv/libuv/blob/17219b8f39f7cd33472c94214010b603322bd0fa/src/unix/process.c#L1086
+    //
+    // or, 2:
+    // https://github.com/nodejs/node/blob/4df34cf6dd497c4b7487c98fa5581c85b05063dc/src/node_process_methods.cc#L146
+    // which calls uv_kill: https://github.com/libuv/libuv/blob/17219b8f39f7cd33472c94214010b603322bd0fa/src/unix/process.c#L1092
+    // which calls kill.
+    /*
+        https://www.gnu.org/software/libc/manual/html_node/Signaling-Another-Process.html
+        The return value from kill is zero if the signal can be sent successfully. Otherwise, no signal is sent, and a value of -1 is returned. If pid specifies sending a signal to several processes, kill succeeds if it can send the signal to at least one of them. There’s no way you can tell which of the processes got the signal or whether all of them did.
+
+        The following errno error conditions are defined for this function:
+
+        EINVAL
+        The signum argument is an invalid or unsupported number.
+
+        EPERM
+        You do not have the privilege to send a signal to the process or any of the processes in the process group named by pid.
+
+        ESRCH
+        The pid argument does not refer to an existing process or group.
+    */
+    // UV__ERR is https://github.com/libuv/libuv/blob/17219b8f39f7cd33472c94214010b603322bd0fa/src/uv-common.h#L47
+
+    // On windows it's ugly (and they don't check all return values!)
+    // https://github.com/libuv/libuv/blob/17219b8f39f7cd33472c94214010b603322bd0fa/src/win/process.c#L1374
+    // Internally it calls uv__kill, which is hella cursed: https://github.com/libuv/libuv/blob/17219b8f39f7cd33472c94214010b603322bd0fa/src/win/process.c#L1158
 }
 
 main().then(
@@ -490,36 +547,40 @@ main().then(
         return ensureClosed(webpack);
     }).then(() => process.exit(2));
 }).finally(() => {
-
+    console.log(`finally (1)`);
     return ensureClosed(cypress_rails).then(() => {
         return ensureClosed(webpack);
     }).then(() => {
-        const is3000Clear = checkPortClear('3000');
+        console.log(`finally (2)`);
+        const is3000Clear = checkPortClear(DEFAULT_RAILS_PORT);
         if (!is3000Clear) {
-            console.log(`port 3000 is not clear!`);
+            console.log(`port ${DEFAULT_RAILS_PORT} is not clear!`);
             return 1;
         }
-        const is3001Clear = checkPortClear('3001');
+        const is3001Clear = checkPortClear(DEFAULT_FRONTEND_PORT);
         if (!is3001Clear) {
-            console.log(`port 3001 is not clear!`);
+            console.log(`port ${DEFAULT_FRONTEND_PORT} is not clear!`);
             return 1;
         }
     
         console.log(`all done!`);
     })
 }).then((result) => {
+    console.log(`finally (3)`);
     // console.assert(!(rails?.isRunning), `${rails?.cmd} is still running!!! See ${rails?.pid}`);
     console.assert(!(cypress_rails?.isRunning), `${cypress_rails?.cmd} is stil running!! see ${cypress_rails?.pid}`);
     console.assert(!(webpack?.isRunning), `${webpack?.cmd} is stil running!! see ${webpack?.pid}`);
 
-    const is3000Clear = checkPortClear('3000');
+    const is3000Clear = checkPortClear(DEFAULT_RAILS_PORT);
     if (!is3000Clear) {
-        console.log(`port 3000 is not clear!`);
+        console.log(`port ${DEFAULT_RAILS_PORT} is not clear!`);
+        forceClose(cypress_rails_pid);
         return 1;
     }
-    const is3001Clear = checkPortClear('3001');
+    const is3001Clear = checkPortClear(DEFAULT_FRONTEND_PORT);
     if (!is3001Clear) {
-        console.log(`port 3001 is not clear!`);
+        console.log(`port ${DEFAULT_FRONTEND_PORT} is not clear!`);
+        forceClose(webpack_pid);
         return 1;
     }
 
